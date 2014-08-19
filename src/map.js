ceb447cebdb23c780a84f5054179a2dbfce9a177
Carl Olsen <carlolsen@Carls-MacBook-Pro.local>
2014-08-19 10:01:27 -0400

    updated Map (map.js) - allow tile param to be string matching Tile.Types[tile] creating a new Tile obj of that type Map.prototype.set(x, y, tile); - added defaultTileType  param to Map.prototype.loadTilesFromArrayString(mapData, charToType, defaultTileType);

--- src/map.js
+++ src/map.js
@@ -23,6 +23,24 @@
         game: null,
 
         /**
+        * Sets a value at given coords.
+        * @method set
+        * @param {Number} x - Position on the x axis of the value being set.
+        * @param {Number} y - Position on the y axis of the value being set.
+        * @param {Tile|String} tile - The Tile being set at given coords. If Tile is a string a new tile will be created using the string as the Tile Type (see Tile.Types[type]).
+        */
+        set: function(x, y, tile) {
+            if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
+                return;
+            }
+            if(typeof tile === 'string'){
+                tile = new RL.Tile(this.game, tile, x, y);
+            }
+
+            this.data[x][y] = tile;
+        },
+
+        /**
         * Checks if a map tile can be seen through.
         * @method canSeeThroughTile
         * @param {Number} x - The x map tile coord to check.
@@ -49,25 +67,29 @@
         /**
         * Loads Tile data from an array of strings
         * @method loadTilesFromArrayString
-        * @param {Array} x - The array of strings to load.
+        * @param {Array} mapData - The array of strings to load.
         * @param {Object} charToType - An object mapping string characters to Tile types (see Tile.Types[type]).
+        * @param {String} [defaultTileType] - The tile type to use if a character is not in charToType. This is used to allow characters representing entites or non-tile objects to be included in the mapData.
         * @example
 
+            // 'P' will be ignored and a floor tile will be placed at that position
             var mapData = [
                 '####',
                 '#..#',
-                '#..#',
+                '#.P#',
                 '####',
             ],
-            // 'wall'
+
             charToType = {
                 '#': 'wall',
                 '.': 'floor'
-            };
-            map.loadTilesFromArrayString(mapData, charToType);
+            },
+            defaultTileType = 'floor';
+
+            map.loadTilesFromArrayString(mapData, charToType, defaultTileType);
         *
         */
-        loadTilesFromArrayString: function(mapData, charToType){
+        loadTilesFromArrayString: function(mapData, charToType, defaultTileType){
             var width = mapData[0].length,
                 height = mapData.length;
 
@@ -78,8 +100,11 @@
             // loop over each coord in the Array2d (val will be undefined)
             this.each(function(val, x, y){
                 var char = mapData[y][x],
-                    type = charToType[char],
-                    tile = new RL.Tile(this.game, type, x, y);
+                    type = charToType[char];
+                if(type === void 0 && defaultTileType){
+                    type = defaultTileType;
+                }
+                var tile = new RL.Tile(this.game, type, x, y);
                 this.add(x, y, tile);
             });
         }
