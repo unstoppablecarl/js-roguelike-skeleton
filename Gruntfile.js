3e13966d5bdb14460df431bafe30f4428eb54546
Carl Olsen <carlolsen@Carls-MacBook-Pro.local>
2014-09-24 12:51:40 -0400

    removed un-used metalsmith-less

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,6 @@
 module.exports = function(grunt) {
 'use strict';
     grunt.initConfig({
-        less: {
-            examples: {
-                src: 'examples/assets/less/main.less',
-                dest: 'examples/assets/css/style.css',
-            },
-            options: {
-                sourceMap: true,
-                sourceMapFilename: 'examples/assets/css/style.css.map',
-                // Override the default url that points to the sourcemap from the compiled css file.
-                sourceMapURL: 'style.css.map',
-            }
-        },
         watch: {
             examples_less: {
                 files: ['examples/assets/less/**/*.less'],
@@ -41,14 +29,12 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-yuidoc');
-    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     var metalsmithTask = require('./manual-src/task.js');
     grunt.registerTask('metalsmith', 'metalsmith build task', metalsmithTask);
 
     grunt.registerTask('build', [
-        'less:examples',
         'yuidoc:examples',
         'metalsmith'
     ]);
